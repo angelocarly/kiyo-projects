5203a6610eb5da6914b4f0c2de46949f110650a3
Angelo Carly <angelo.carly@protonmail.com>
2024-08-15 22:31:01 +0200

    Add base fractal-folding

---
+++ fractal-folding/README.md
@@ -0,0 +1,3 @@
+# Fractal folding
+
+Exploration into fractal folding, using this site as a guide: http://roy.red/posts/folding-the-koch-snowflake/
---
+++ fractal-folding/src/main.rs
@@ -0,0 +1,25 @@
+use kiyo::app::app::{App, AppConfig};
+use kiyo::app::draw_orch::{DispatchConfig, DrawConfig, Pass};
+
+fn main() {
+
+    let app_config = AppConfig {
+        width: 1000,
+        height: 1000,
+        vsync: true,
+        log_fps: true,
+    };
+    let app = App::new(app_config);
+
+    let mut draw_config = DrawConfig::new();
+    draw_config.passes = Vec::from([
+        Pass {
+            shader: "fractal-folding/shaders/fractal.comp".to_string(),
+            dispatches: DispatchConfig::FullScreen,
+            input_resources: Vec::from( [ 0 ]),
+            output_resources: Vec::from([ 0 ]),
+        },
+    ]);
+
+    app.run(draw_config);
+}
