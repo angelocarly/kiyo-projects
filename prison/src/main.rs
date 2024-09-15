026aae30702ff9276e93f4dbe8395ca1433b1381
Angelo Carly <angelo.carly@protonmail.com>
2024-09-15 21:31:34 +0200

    Add prison project

---
+++ prison/src/main.rs
@@ -0,0 +1,30 @@
+use kiyo::app::app::{App, AppConfig};
+use kiyo::app::draw_orch::{ClearConfig, DispatchConfig, DrawConfig, ImageConfig, Pass};
+
+fn main() {
+
+    let app = App::new(AppConfig {
+        width: 1000,
+        height: 1000,
+        vsync: true,
+        log_fps: false,
+    });
+
+    let config = DrawConfig {
+        images: Vec::from([
+            ImageConfig {
+                clear: ClearConfig::None
+            },
+        ]),
+        passes: Vec::from([
+            Pass {
+                shader: "prison/shaders/color.comp".to_string(),
+                dispatches: DispatchConfig::FullScreen,
+                input_resources: Vec::from([ 0 ]),
+                output_resources: Vec::from([ 0 ]),
+            },
+        ])
+    };
+
+    app.run(config, None);
+}
